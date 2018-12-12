9d6cd7a7eadaeb18bd51351f0ec3b32a41c89aad
Kalin Varbanov <kalin.varbanov@gmail.com>
2018-12-12 17:53:08 +0200

    setup CI/CD

---
+++ test_manage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import unittest
+
+from manage import *
+
+
+class TestManage(unittest.TestCase):
+    ''' docstring here'''
+
+    def setUp(self):
+        '''Create a standard 52-card deck to be used for testing '''
+        pass
+
+    def test_placeholder(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
